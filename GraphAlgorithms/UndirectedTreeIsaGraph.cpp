a3bab136725d95927f0d99e3b2d9c66a86b5959b
ranahiten8 <ranahiten8@gmail.com>
2021-11-26 12:12:21 +0530

    update

--- GraphAlgorithms/UndirectedTreeIsaGraph.cpp
+++ GraphAlgorithms/UndirectedTreeIsaGraph.cpp
@@ -17,10 +17,59 @@ public:
         l[x].push_back(y);
         l[y].push_back(x);
     }
+    bool is_tree()
+    {
+        bool *visited = new bool[V];
+        int *parent = new int[V];
+
+        queue<int> q;
+
+        for (int i = 0; i < V; ++i)
+        {
+            visited[i] = false;
+            parent[i] = i;
+        }
+
+        int src = 0;
+        q.push(src);
+        visited[src] = true;
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            for (int nbr : l[node])
+            {
+                if (visited[nbr] == true and parent[node] != nbr)
+                {
+                    return false;
+                }
+                else if (!visited[nbr])
+                {
+                    visited[nbr] = true;
+                    parent[nbr] = node;
+                    q.push(nbr);
+                }
+            }
+        }
+        return true;
+    }
 };
- 
+
 int main()
 {
+
+    Graph g(4);
+
+    g.addEdge(0, 1);
+    g.addEdge(3, 2);
+    g.addEdge(1, 2);
+    // g.addEdge(0, 3);
+
+    if (g.is_tree())
+        cout << "yes it is a tree";
+    else
+        cout << "no its a graph";
 
     return 0;
 }
